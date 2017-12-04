51de57bce1904d3d18f64004d59b1587c79bcffb
millo <millo@192.168.1.16>
2017-12-04 00:03:08 -0300

    Se agrego las funcionalidades SQL para el repositorio pacientes

--- src/datos/RepositorioPacienteMySQL.java
+++ src/datos/RepositorioPacienteMySQL.java
@@ -3,27 +3,25 @@ package datos;
 import dominio.contratosRepositorios.RepositorioObraSocial;
 import dominio.contratosRepositorios.RepositorioPaciente;
 import dominio.entidades.AfiliacionObraSocial;
+import dominio.entidades.DBConection;
 import dominio.entidades.Paciente;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import main.FactoriaDeConfiguracion;
 
-import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RepositorioPacienteMySQL implements RepositorioPaciente {
 
-    private ArrayList<Paciente> pacientes;
+    private Connection conexion;
+    private final RepositorioObraSocial repositorioObraSocial;
 
     public RepositorioPacienteMySQL() {
-        RepositorioObraSocial repositorioObraSocial = FactoriaDeConfiguracion.getRepositorioObraSocial();
-        inicializarPacientes(repositorioObraSocial);
-    }
-
-    private void inicializarPacientes(RepositorioObraSocial repositorioObraSocial) {
-
-        pacientes = new ArrayList<>();
-        agregarPaciente(nuevoPaciente("Federico", "Fia Sare", "Alderetes", 37754244, 3814785477L, 24, new AfiliacionObraSocial(repositorioObraSocial.obtenerObraSocial(0), 0)));
-        agregarPaciente(nuevoPaciente("Emmanuel", "Gonzalez", "San Miguel", 38487359, 3815554789L, 23, new AfiliacionObraSocial(repositorioObraSocial.obtenerObraSocial(1), 2)));
-        agregarPaciente(nuevoPaciente("Gerardo", "Mansilla", "San Miguel", 36838004, 3816875478L, 25, new AfiliacionObraSocial(repositorioObraSocial.obtenerObraSocial(2), 3)));
-        agregarPaciente(nuevoPaciente("Rodrigo", "Sanchez", "San Miguel", 37315082, 3815879444L, 24, new AfiliacionObraSocial(repositorioObraSocial.obtenerObraSocial(0), 4)));
+        repositorioObraSocial = FactoriaDeConfiguracion.getRepositorioObraSocial();
+        conexion = DBConection.iniciarConexion();
     }
 
     private Paciente nuevoPaciente(String nombre, String apellido, String domicilio, int dni, long telefono, int edad, AfiliacionObraSocial obraSocial) {
@@ -32,15 +30,37 @@ public class RepositorioPacienteMySQL implements RepositorioPaciente {
 
     @Override
     public Paciente buscarPaciente(int dni) {
-        for (Paciente p : pacientes) {
-            if (p.getDni() == dni)
-                return p;
+        String sql = "Select * from pacientes where dni = " + dni;
+        try {
+            Statement st = conexion.createStatement();
+            ResultSet res = st.executeQuery(sql);
+            if (res.first()) {
+                String nombre = res.getString("nombre");
+                String apellido = res.getString("apellido");
+                String domicilio = res.getString("direccion");
+                Long telefono = res.getLong("telefono");
+                int edad = res.getInt("edad");
+                Paciente paciente = nuevoPaciente(nombre, apellido, domicilio, dni, telefono, edad, new AfiliacionObraSocial(repositorioObraSocial.obtenerObraSocial(0), 0));
+                st.close();
+                return paciente;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RepositorioPacienteMySQL.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
 
     @Override
     public void agregarPaciente(Paciente paciente) {
-        pacientes.add(paciente);
+        String sql = "INSERT INTO `pacientes`(`dni`, `nombre`, `apellido`, `direccion`, `telefono`, `edad`) VALUES ("+
+                paciente.getDni() + ",'" + paciente.getNombre() + "','" +
+                paciente.getApellido() + "','" + paciente.getDomicilio() + "'," +
+                paciente.getTelefono() + "," + paciente.getEdad() + ")";
+        try {
+            Statement st = conexion.createStatement();
+            st.execute(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(RepositorioPacienteMySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
